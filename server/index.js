137b9830c3fd65648638644e1183cc0f5246be69
Debangan Deb <debangandeb14@gmail.com>
2025-06-09 15:53:08 +0530

    Update server package.json and index.js

--- server/index.js
+++ server/index.js
@@ -22,4 +22,4 @@ app.get('/', (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
