3f94b00a4b1f4911e3f1fb9f5ad0cee5769a961d
Debangan Deb <debangandeb14@gmail.com>
2025-06-10 17:36:45 +0530

    improved small issues

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,18 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
   return (
-    <section className="my-5 py-5">
+    <section className="my-5 py-5" >
       <div className="text-center my-5">
         <h1 className="fw-bold m-3">
           Hi, I'm <span className="text-primary">Debangan Deb</span>
         </h1>
         <p className="lead text-info mb-4">Fresher</p>
 
-        <div className="d-flex justify-content-center gap-3 mb-4 ">
-          <a href="/Resume.pdf" className="btn btn-primary text-black fw-bold" download>
+        <div className="d-flex flex-column flex-md-row align-items-center justify-content-center gap-3 mb-4 ">
+          <a href="/Resume.pdf" className="btn btn-primary text-black fw-bold col-6 col-md-auto" download>
             Download Resume
           </a>
-          <a href="/contact" className="btn btn-outline-primary fw-bold">
+          <a href="/contact" className="btn btn-outline-primary fw-bold col-6 col-md-auto">
             Contact Me
           </a>
         </div>
